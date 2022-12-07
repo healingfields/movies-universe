a6816698b809e7e896877130461219f3b7b845f3
healingfields <dihratene@gmail.com>
2022-12-07 20:40:15 +0100

    initialized the spring boot project

---
+++ backend/src/main/java/ma/showmaker/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package ma.showmaker.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
